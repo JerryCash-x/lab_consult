dd79f15f94c1dff422b8436cfd5bcf7c341a3db3
Macebre <macer7747@gmail.com>
2019-01-08 00:30:46 +0200

    jqueryFunctional

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
+
 $(function() {
-    function doMagic() {
-        var btn = $('.btn');
-        btn.click(function() {
-            var lis = $('.menu__item a');
-            lis.toggleClass('red');
-        });
-    };
 
-    doMagic();
+    // Preloader
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+        $spinner.fadeOut();
+        $preloader.delay(3050).fadeOut('slow');
+ 
+    // Hamburger
+    $('.menu-open').click(function () {
+        $('.menu-collapse').toggleClass('d-none').css('order', '1');
+        $('.menu').toggleClass('menu-opened');
+    })
+    $('.menu-opened li a').click(function () {
+        $('.menu-collapse').toggleClass('d-none').css('order', '1');
+        $('.menu').toggleClass('menu-opened');
+    })
 
-    $('add-btn').click(function() {
-        $('li').before('<p>Spaaaaaan</p>')
-    });
+    //  To make img a full screen
 
-    $('#accordion').accordion();
+    // $('.gallery__wrap').click(function(){
+    //     var img = $('.gallery__img'); 
+    //     var src = img.attr('src'); 
+    //     $('.gallery').css('position', 'relative').append("<div class='popup_bg'><img src='"+src+"' class='popup_img'></div>");
+    //     $('.gallery__wrap').addClass('popup_bg');
+    //     $('.popup_bg').fadeIn(800).click(function (){
+    //         $(".gallery__img").fadeOut(800);
+    //     })
+    // });  
 
-    $('#tabs').tabs();
+    // Slider
+
+    $('.content__slider').slick({
+        dots:true,
+        prevArrow:'<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow:'<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>'
+    });  
 
 });
 
+        
 
 
 
@@ -32,13 +53,3 @@ $(function() {
 
 
 
-
-
-
-
-
-
-
- // $('a').css('border', '2px solid red')
-    // .add('.p')
-    // .css('background', 'yellow')
